3478d074a9ee986eea317990dfb5cc678e3fa4f4
Euan Murray <euan.murray@itftennis.com>
2019-05-07 15:34:22 +0100

    Added product list and search

---
+++ PTC/PTCData/TrainingProductViewModel.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+
+namespace PTCData
+{
+    public class TrainingProductViewModel
+    {
+        public TrainingProductViewModel()
+        {
+            Products = new List<TrainingProduct>();
+            SearchEntity = new TrainingProduct();
+            EventCommand = "List";
+        }
+
+        public string EventCommand { get; set; }
+        public List<TrainingProduct> Products { get; set; }
+        public TrainingProduct SearchEntity { get; set; }
+
+        private void ResetSearch()
+        {
+            SearchEntity = new TrainingProduct();
+        }
+
+        public void HandleRequest()
+        {
+            switch (EventCommand.ToLower())
+            {
+                case "list":
+                case "search":
+                    Get();
+                    break;
+
+                case "resetsearch":
+                    ResetSearch();
+                    Get();
+                    break;
+
+                default:
+                    break;
+            }
+        }
+
+        private void Get()
+        {
+            TrainingProductManager mgr = new TrainingProductManager();
+
+            Products = mgr.Get(SearchEntity);
+        }
+    }
+}
